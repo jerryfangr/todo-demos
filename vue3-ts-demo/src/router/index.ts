6a801443cd2c7e0dba38aa37c14ccb4713177bc2
jerryfangr <2a15068517423@gmail.com>
2021-06-15 21:32:17 +0800

    add 404 match

--- vue3-ts-demo/src/router/index.ts
+++ vue3-ts-demo/src/router/index.ts
@@ -13,20 +13,39 @@ const routes: Array<RouteRecordRaw> = [
     name: 'home',
     component: Home
   },
-  {
+  { // todolist detail
     path: '/todolist/:name',
     name: 'todolist',
     component: Home,
-    children: [{
-      path: '/todoitem/:id',
+    props: true,
+    meta: { needLoadData: true },
+
+    children: [{ // todoitem detail
+      path: 'todoitem/:id',
       component: Home,
+      props: true,
     }]
+  },
+  { // not match paths
+    path: '*',
+    name: '404',
+    redirect: (to) => {
+      return '/home';
+    }
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.needLoadData) {
+    if (to) {
+      
+    }
+  }
 })
 
 export default router
