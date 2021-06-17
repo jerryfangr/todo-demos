8d5cd901d3d78078f00640aeb796b3b56de8091b
jerryfangr <2a15068517423@gmail.com>
2021-06-17 19:09:52 +0800

    add click event listener(edit and delete)

--- vue3-ts-demo/src/components/TodoCard.vue
+++ vue3-ts-demo/src/components/TodoCard.vue
@@ -1,62 +1,135 @@
 <template>
-  <div class="card"     
+  <div 
+    class="card"
+    :style="cardStyle"   
     @mouseover="showButton=true"
     @mouseleave="showButton=false"
+    @click="editItem"
   >
-    {{ 'title' }}
 
-    <n-space v-if="showButton">
+    <div class="text">
+        <slot></slot>
+    </div> 
 
-      <n-button @click="deleteItem">
-        <template #icon>
-          <n-icon>
-            <edit-icon />
-          </n-icon>
-        </template>
+    <div class="operate" v-show="showButton">
+      <n-button 
+        size="tiny"
+        @click="deleteItem"
+        color="#FFF"
+        :loading="openLoading"
+      >
+        <n-icon v-if="!openLoading"><delete-icon /></n-icon>
       </n-button>
+    </div>
 
-      <n-button icon-placement="right">
-        <template #icon>
-          <n-icon>
-            <delete-icon />
-          </n-icon>
-        </template>
-      </n-button>
-
-    </n-space>    
   </div>
 </template>
 
 <script setup lang="ts">
-  import { NSpace, NButton, NIcon } from 'naive-ui'
+  import { NButton, NIcon } from 'naive-ui'
   import { Delete28Regular as DeleteIcon, DocumentEdit16Regular as EditIcon } from '@vicons/fluent'
-  import { ref, useContext  } from 'vue'
+  import { ref, useContext, defineProps  } from 'vue'
   // import { getCurrentInstance  } from 'vue'
   // import type { ComponentInternalInstance  } from 'vue'
   // const { emit } = getCurrentInstance() as ComponentInternalInstance 
 
-
+  defineProps({
+    openLoading: {
+      type: Boolean,
+      default: false
+    }, 
+    cardStyle: {
+      type: String,
+      default: ''
+    }
+  })
 
   ref: showButton = false
 
   const { emit } = useContext()
 
-  const deleteItem = () => {
-    emit('deleteEvent');
+  ref: hasDeleted = false
+
+  // click delete button event
+  const deleteItem = (e: MouseEvent) => {
+    e.stopPropagation()
+    if (!hasDeleted) {
+      hasDeleted = true
+      emit('deleteEvent')
+    }
+  }
+
+  // click edit button event
+  const editItem = () => {
+    emit('editEvent');
   }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
   .card {
+    box-sizing: border-box;
     display: inline-flex;
-    justify-content: flex-start;
+    justify-content: space-between;
     align-items: center;
-    flex-wrap: nowrap;
-    border: 1px solid #e4e4e4;
-    color: #37352f;
-    font-size: 1.6em;
-    width: 100%;
-    text-indent: 1em;
-    // background: #ccc;
+    border: 1px solid #dfdfdf;
+    color: #585856;
+    font-size: 16px;
+    font-weight: bold;
+    height: 40px;
+    width: 260px;
+    transition: background .2s;
+    border-radius: 3px;
+    padding: 0 12px;
+    box-shadow: 0 0 2px 0 #d2d2d2;
+    
+    &:hover {
+      background: #f9f9f8;
+      cursor: pointer;
+    }
+
+    div {
+      margin: 0;
+      padding: 0;
+    }
+    
+    .text {
+      flex-grow: 1;
+      text-align: left;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+
+    .operate {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      animation: appear .3s ease-in-out;
+      & ::v-deep(.n-button) {
+        background: #fff;
+        color: #575654;
+        border: 1px solid #d1d1d1;
+        height: 26px;
+        width: 26px;
+        box-shadow: 0 0 2px 0 #d1d1d1;
+
+        .n-button__icon {
+          margin: 0;
+        }
+
+        &:hover {
+          background: #efefee;
+        }
+      }
+    }
+
+
+    @keyframes appear {
+      0% { 
+        opacity: 0; 
+      }
+      100% { 
+        opacity: 1; 
+      }
+    }
   }
 </style>
