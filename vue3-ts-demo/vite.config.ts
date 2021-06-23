d9524ce7bd11aad96bdcf85140592b3bee69886f
jerryfangr <2a15068517423@gmail.com>
2021-06-23 14:41:05 +0800

    update vite.config

--- vue3-ts-demo/vite.config.ts
+++ vue3-ts-demo/vite.config.ts
@@ -1,27 +1,47 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfigExport, ConfigEnv } from 'vite'
+import viteCompression from 'vite-plugin-compression'
+import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-    rollupOptions: {
-      external: ["vue"],
-    },
-  },
-  resolve: {
-    alias: [{
-      find: '@',
-      replacement: resolve(__dirname, './src')
-    }],
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@import "./src/styles/common";'
-      }
-    }
+export default ({ command, mode }: ConfigEnv): UserConfigExport => {
+  const isProduction = mode === 'production'
+  const plugins = [vue()]
+
+  if (isProduction) {
+    plugins.push(
+      legacy({
+        targets: ['ie >= 11'],
+        additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+      }),
+      // viteCompression() // compress file to gzip
+    )
   }
 
-})
+  return defineConfig({
+    base: './',
+    plugins,
+    resolve: {
+      alias: [{
+        find: '@',
+        replacement: resolve(__dirname, './src')
+      }],
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "./src/styles/common";'
+        }
+      }
+    },
+    build: {
+      target: 'es2015',
+      cssCodeSplit: true,
+      minify: !isProduction ? 'esbuild' : 'terser',
+      terserOptions: {
+        compress: { drop_console: isProduction}
+      }
+    }
+  })
+}
+
