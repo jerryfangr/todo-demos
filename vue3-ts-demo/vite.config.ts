760f15aec083e379b9166252795fd547367b94e1
jerryfangr <2a15068517423@gmail.com>
2021-06-23 13:42:52 +0800

    update vite config

--- vue3-ts-demo/vite.config.ts
+++ vue3-ts-demo/vite.config.ts
@@ -5,6 +5,11 @@ import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      external: ["vue"],
+    },
+  },
   resolve: {
     alias: [{
       find: '@',
